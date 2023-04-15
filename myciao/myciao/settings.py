d7235a99f219e1559b81b3cabb2215b435376f49
Diego Andrade <diego.andrade@udc.es>
2023-04-15 08:11:18 +0200

    Small fixes

--- myciao/myciao/settings.py
+++ myciao/myciao/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'reviews',
      'crispy_forms',
+     'crispy_bootstrap4',
 
 ]
 
@@ -129,3 +130,5 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
